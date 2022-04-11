a4c766d52fb615bd5592b4b62f06bc04607a9ad4
snowjak88 <sjnowak88@gmail.com>
2022-04-11 15:26:27 -0400

    Initial SQL script has bad syntax

--- src/main/resources/db/migration/V1__InitialLoad.sql
+++ src/main/resources/db/migration/V1__InitialLoad.sql
@@ -1,30 +1,30 @@
-create table [Scopes] (
-    [id] int identity(1,1) primary key,
-    [version] int not null,
-    [name] nvarchar(255) not null unique
+create table `Scopes` (
+    id int identity(1,1) primary key,
+    version int not null,
+    name nvarchar(255) not null unique
 );
 
-create table [Users] (
-    [id] int identity(1,1) primary key,
-    [version] int not null,
-    [username] nvarchar(255) not null unique,
-    [password] nvarchar(255) not null,
-    [created] datetime not null
+create table `Users` (
+    id int identity(1,1) primary key,
+    version int not null,
+    username nvarchar(255) not null unique,
+    password nvarchar(255) not null,
+    created datetime not null
 );
 
-create table [User_Scopes] (
-    [userID] int not null,
-    [scopeID] int not null,
-    foreign key ( [userID] ) references [Users] ( [id] ),
-    foreign key ( [ScopeID] ) references [Scopes] ( [id] )
+create table `User_Scopes` (
+    `userID` int not null,
+    scopeID int not null,
+    foreign key ( userID ) references `Users` ( id ),
+    foreign key ( scopeID ) references `Scopes` ( id )
 );
 
-insert into [Scopes] ( [version], [name] ) values ( 1, 'browse' );
-insert into [Scopes] ( [version], [name] ) values ( 1, 'write' );
-insert into [Scopes] ( [version], [name] ) values ( 1, 'delete' );
-insert into [Scopes] ( [version], [name] ) values ( 1, 'browse-all' );
-insert into [Scopes] ( [version], [name] ) values ( 1, 'write-all' );
-insert into [Scopes] ( [version], [name] ) values ( 1, 'delete-all' );
+insert into `Scopes` ( version, name ) values ( 1, 'browse' );
+insert into `Scopes` ( version, name ) values ( 1, 'write' );
+insert into `Scopes` ( version, name ) values ( 1, 'delete' );
+insert into `Scopes` ( version, name ) values ( 1, 'browse-all' );
+insert into `Scopes` ( version, name ) values ( 1, 'write-all' );
+insert into `Scopes` ( version, name ) values ( 1, 'delete-all' );
 
 --
 -- Admin user.
@@ -34,10 +34,10 @@ insert into [Scopes] ( [version], [name] ) values ( 1, 'delete-all' );
 --   * iterations: 50,000
 --   * hash-width: 256
 --   * Cipher: PBKDF2WithHmacSHA1
-insert into [Users] ( [version], [username], [password], [created] ) values ( 1, 'admin', 'jQd6X8yaNdQjjNJwN/bqU4roqgVkXEWBY++iP5D6aig=', getdate() );
-insert into [User_Scopes] ( [userID], [scopeID] )
-select u.[id], s.[id]
-    from [Users] u
-    cross join [Scopes] s
-    where u.[username] = 'admin'
-        and s.[name] in ( 'browse', 'write', 'delete', 'browse-all', 'write-all', 'delete-all' );
+insert into `Users` ( version, username, password, created ) values ( 1, 'admin', 'jQd6X8yaNdQjjNJwN/bqU4roqgVkXEWBY++iP5D6aig=', getdate() );
+insert into `User_Scopes` ( userID, scopeID )
+select u.id, s.id
+    from `Users` u
+    cross join `Scopes` s
+    where u.username = 'admin'
+        and s.name in ( 'browse', 'write', 'delete', 'browse-all', 'write-all', 'delete-all' );
